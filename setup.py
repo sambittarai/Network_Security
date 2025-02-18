06dc41cffd3bbff587789478f1f458f81fa96d4e
Sambit <sambitarai17@gmail.com>
2025-02-18 17:23:25 +0100

    Exception handling and logging completed

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
 
 from setuptools import find_packages, setup
 from typing import List
